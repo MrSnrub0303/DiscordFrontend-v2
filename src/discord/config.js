65af01349747c2149bd57ce0477e32c62459cadd
Emeka Ugbanu <emekaugbanu@gmail.com>
2025-11-15 07:19:15 +0100

    update-F

--- src/discord/config.js
+++ src/discord/config.js
@@ -1,2 +1,2 @@
-export const DISCORD_CLIENT_ID ='1414187165146943518';
+export const DISCORD_CLIENT_ID ='1438780052144783371';
 export const ACTIVITY_URL = 'https://discordbackend-xggi.onrender.com'; 
